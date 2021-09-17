a9c5865e18dc4caf1f93cd9860372df5d05fc995
artyom k <artyomcarman@gmail.com>
2021-09-17 18:53:44 +0400

    c57aa18

---
+++ _nuxt/static/1631889203/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fshiny-pancake\u002F_nuxt\u002Fstatic\u002F1631889203",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fshiny-pancake\u002F",assetsPath:"\u002Fshiny-pancake\u002F_nuxt\u002F",cdnURL:a}}}}(null));
